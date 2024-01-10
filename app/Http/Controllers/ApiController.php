90c80545af67b02d5eed0ef172dded32505c8a6f
ihossain5 <ismail.hossain5169@gmail.com>
2024-01-10 22:08:25 +0600

    CI/CD checking

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -28,7 +28,7 @@ class ApiController extends Controller
     public function sendMessage(MessageRequest $request){
         Message::create($request->validated());
 
-        Mail::to('ismail.hossain1460@gmail.com')->send(new MessageMail($request->all()));
+        Mail::to(env('DEFAULT_MAIL'))->send(new MessageMail($request->all()));
 
         return true;
     }
